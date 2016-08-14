af01e544441cb3c8a0621faa6179cf38b88e4126
Vova Trentovskyj <trentovskyj@gmail.com>
2016-08-14 18:04:12 +0300

    data changes

--- src/test/java/TernopolKyivPackage/TernKyivTest.java
+++ src/test/java/TernopolKyivPackage/TernKyivTest.java
@@ -62,12 +62,12 @@ public class TernKyivTest {
         driver.findElement(By.name("station_till")).sendKeys(Keys.DOWN);
         driver.findElement(By.name("station_till")).sendKeys(Keys.ENTER);
         driver.findElement(By.id("date_dep")).click();
-        driver.findElement(By.xpath("//td[@data-month='6']//a[text()='29']")).click();
+        driver.findElement(By.xpath("//td[@data-month='7']//a[text()='21']")).click();
         //driver.findElement(By.linkText("7")).click();
 
         try {
             myDynamicElement.until(ExpectedConditions.invisibilityOfElementLocated(
-                    By.xpath("//td[@data-month='6']//a[text()='29']")));
+                    By.xpath("//td[@data-month='7']//a[text()='21']")));
         } catch (Exception e) {
             System.out.println("Somthing wrong :(");
         }
